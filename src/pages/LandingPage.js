f278bde04fd7addc48e0b7a05147976e965e963d
Ansharullah Widiansyah <wanshar9@gmail.com>
2022-09-07 01:20:58 +0700

    Fix misstyped class + complete component parts/footer

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,9 +3,11 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
 import landingPage from "json/landingPage.json";
-import Categories from "parts/Categories";
 export default class LandingPage extends Component {
   constructor(props) {
     super(props);
@@ -21,6 +23,8 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked}
         />
         <Categories data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial} />
+        <Footer />
       </>
     );
   }
